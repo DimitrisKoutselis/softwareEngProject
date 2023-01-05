0e792d93086a9cf147e36b22bfdd6d6d58d69f43
ShingekiNoKyoj1n <andrewkurosaki@gmail.com>
2023-01-05 19:46:27 +0200

    tralala

---
+++ src/RegisterForm.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class RegisterForm {
+    private JLabel registerFormLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+    }
+}
---
+++ src/createTeamForm.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class createTeamForm {
+    private JLabel createTeamLabel;
+    private JLabel teamNameLabel;
+    private JTextField teamNameTextField;
+    private JLabel teamPointsLabel;
+    private JTextField teamPointsTextField;
+    private JLabel teamIconLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
