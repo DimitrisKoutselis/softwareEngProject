af022eb4c044980b9d0878a4d83086dbe6587663
DimitrisKoutselis <dimitriskoytselis@gmail.com>
2023-01-05 12:26:37 +0200

    01/05/2022_Koutselis

---
+++ src/AdminDeleteUser.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminDeleteUser {
+    private JTextField userIDtextfield;
+    private JButton cancelButton;
+    private JButton confirmButton;
+
+    public AdminDeleteUser() {
+        confirmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
---
+++ src/enterPassowrdForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class enterPassowrdForm {
+    private JPasswordField enterPasswordField;
+    private JButton confirmButton;
+
+    public enterPassowrdForm() {
+        confirmButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
