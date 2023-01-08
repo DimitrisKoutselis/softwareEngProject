40d0e5a556e3112b2b6f345b8452bca588eb3072
YourAlienFriend <manolisvellis@gmail.com>
2023-01-08 20:32:13 +0200

    Created LogIn and upodate from realtime ui forms

---
+++ src/RealTimeUpForm.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class RealTimeUpForm {
+    private JPanel panel1;
+    private JLabel HomeTeam;
+    private JLabel AwayTeam;
+    private JLabel awayScore;
+    private JLabel homeScore;
+    private JLabel Timer;
+    private JTable HomeSquad;
+    private JTable table2;
+    private JScrollPane AwaySquad;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
