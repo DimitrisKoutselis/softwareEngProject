d57c664b18dfbc0e7b3c2479cac0b86f46e83348
YourAlienFriend <manolisvellis@gmail.com>
2023-01-08 23:31:46 +0200

    Created LogIn and upodate from realtime ui forms

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 public class MAIN {
+    public ArrayList<Player> players = new ArrayList<Player>();
+    public ArrayList<Team> teams = new ArrayList<Team>();
 
 
     public static void main(String[] args) {
-        ArrayList<Player> players = new ArrayList<Player>();
-        ArrayList<Team> teams = new ArrayList<Team>();
         ArrayList<User> users = new ArrayList<User>();
         users.add(new Admin(1,"Admin"));
         users.add(new Secretary(2, "Secretary"));
@@ -13,5 +13,16 @@ public class MAIN {
         new createTeamForm();
 
 
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
+    }
+    public void removePlayer(Player player){
+        for (int i=0;i<players.size();i++){
+            if (players.get(i).equals(player)){
+                players.remove(i);
+            }
+        }
     }
 }
